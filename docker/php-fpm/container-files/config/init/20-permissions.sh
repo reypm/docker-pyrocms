139c22ed6ca31968918de8c41fd51a0a02eacee2
mmi-rperez <rperez@managedmaint.com>
2017-01-27 09:05:43 -0500

    Fixing issues with permission script

--- docker/php-fpm/container-files/config/init/20-permissions.sh
+++ docker/php-fpm/container-files/config/init/20-permissions.sh
@@ -6,9 +6,9 @@ set -e
 printf "\n===============Setting up permissions started===============\n"
 usermod -u 1000 apache && groupmod -g 1000 apache && \
 chown -R apache:root /data/www && \
-find /data/pyrocms/public/app -type d -print0 -exec chmod 777 {} \; && \
-find /data/pyrocms/storage -type d -print0 -exec chmod 777 {} \; && \
-find /data/pyrocms/bootstrap/cache -type d -print0 -exec chmod 777 {} \; && \
+find /data/www/pyrocms/public/app -type d -print0 -exec chmod 777 {} \; && \
+find /data/www/pyrocms/storage -type d -print0 -exec chmod 777 {} \; && \
+find /data/www/pyrocms/bootstrap/cache -type d -print0 -exec chmod 777 {} \; && \
 printf "\n===============Setting up permissions finished=============\n"
 
 exec "$@"
